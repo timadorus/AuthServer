59533d8a50fce5749346d66ca8366af22888f127
Hendrik Albers <hendrik.albers@haw-hamburg.de>
2014-10-27 11:03:43 +0100

    added runscripts, updated ant build config
    4 scripts to run and control the server have been added to the project.
    Added an ant target in the build.xml that copies the scripts to the release folder and makes them executable.

---
+++ runscripts/restartAuthServer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/bin/sh /usr/share/timadorus-auth-server/stopAuthServer.sh
+/bin/sleep 3
+/bin/sh /usr/share/timadorus-auth-server/startAuthServer.sh
+
---
+++ runscripts/startAuthServer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+java -jar /usr/share/timadorus-auth-server/auth-server.jar /usr/share/timadorus-auth-server/
+
