3cc3439adb49ef8c8a6aaf41e80a2ac78f017764
Hendrik Albers <hendrik.albers@haw-hamburg.de>
2014-11-03 12:00:24 +0100

    jar will be started from specified working directory now

--- etc/init.d/authserver.sh
+++ etc/init.d/authserver.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 SERVICE_NAME=TimadorusAuthServer
+WORKING_DIR=/usr/share/timadorus-auth-server
 PATH_TO_JAR=/usr/share/timadorus-auth-server/auth-server.jar
 JAR_PARAMS=/usr/share/timadorus-auth-server/
 PID_PATH_NAME=/tmp/TimadorusAuthServer-pid
@@ -8,6 +9,7 @@ case $1 in
     start)
         echo "Starting $SERVICE_NAME ..."
         if [ ! -f $PID_PATH_NAME ]; then
+            cd $WORKING_DIR
             nohup java -jar $PATH_TO_JAR $JAR_PARAMS /tmp 2>> $LOG_FILE >> $LOG_FILE &
             echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
@@ -34,6 +36,7 @@ case $1 in
             echo "$SERVICE_NAME stopped ...";
             rm $PID_PATH_NAME
             echo "$SERVICE_NAME starting ..."
+            cd $WORKING_DIR
             nohup java -jar $PATH_TO_JAR $JAR_PARAMS /tmp 2>> $LOG_FILE >> $LOG_FILE &
             echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
@@ -50,6 +53,7 @@ case $1 in
             rm $PID_PATH_NAME
         fi
         echo "$SERVICE_NAME starting ..."
+        cd $WORKING_DIR
         nohup java -jar $PATH_TO_JAR $JAR_PARAMS /tmp 2>> $LOG_FILE >> $LOG_FILE &
         echo $! > $PID_PATH_NAME
         echo "$SERVICE_NAME started ..."
