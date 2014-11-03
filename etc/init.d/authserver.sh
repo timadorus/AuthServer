ecc8b8bf0794d6b00f1ea579f2263ada31acc51f
Hendrik Albers <hendrik.albers@haw-hamburg.de>
2014-11-03 10:03:40 +0100

    log the output of server jar to a file

--- etc/init.d/authserver.sh
+++ etc/init.d/authserver.sh
@@ -3,12 +3,13 @@ SERVICE_NAME=TimadorusAuthServer
 PATH_TO_JAR=/usr/share/timadorus-auth-server/auth-server.jar
 JAR_PARAMS=/usr/share/timadorus-auth-server/
 PID_PATH_NAME=/tmp/TimadorusAuthServer-pid
+LOG_FILE=authserver.log
 case $1 in
     start)
         echo "Starting $SERVICE_NAME ..."
         if [ ! -f $PID_PATH_NAME ]; then
-            nohup java -jar $PATH_TO_JAR $JAR_PARAMS /tmp 2>> /dev/null >> /dev/null &
-                        echo $! > $PID_PATH_NAME
+            nohup java -jar $PATH_TO_JAR $JAR_PARAMS /tmp 2>> $LOG_FILE >> $LOG_FILE &
+            echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
         else
             echo "$SERVICE_NAME is already running ..."
@@ -33,8 +34,8 @@ case $1 in
             echo "$SERVICE_NAME stopped ...";
             rm $PID_PATH_NAME
             echo "$SERVICE_NAME starting ..."
-            nohup java -jar $PATH_TO_JAR $JAR_PARAMS /tmp 2>> /dev/null >> /dev/null &
-                        echo $! > $PID_PATH_NAME
+            nohup java -jar $PATH_TO_JAR $JAR_PARAMS /tmp 2>> $LOG_FILE >> $LOG_FILE &
+            echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
         else
             echo "$SERVICE_NAME is not running ..."
@@ -49,7 +50,7 @@ case $1 in
             rm $PID_PATH_NAME
         fi
         echo "$SERVICE_NAME starting ..."
-        nohup java -jar $PATH_TO_JAR $JAR_PARAMS /tmp 2>> /dev/null >> /dev/null &
+        nohup java -jar $PATH_TO_JAR $JAR_PARAMS /tmp 2>> $LOG_FILE >> $LOG_FILE &
         echo $! > $PID_PATH_NAME
         echo "$SERVICE_NAME started ..."
     ;;
